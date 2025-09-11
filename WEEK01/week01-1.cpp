8f81dc18092932a8bd2bc4bd9399b7c2b73a7adc
cindytytt <cindytytt@gmail.com>
2025-09-11 17:42:34 +0800

    WEEK01

---
+++ WEEK01/week01-1.cpp
@@ -0,0 +1,13 @@
+//LeetCode 28. Find the Index of the First Occurrence in a String
+//week01-1
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int H = haystack.length(), N = needle.length();
+        for(int i=0; i<=H - N; i++){
+            if(haystack.substr(i,N)== needle) return i;
+        }
+        return -1;
+    }
+
+};
---
+++ WEEK01/week01-2b.py
@@ -0,0 +1,5 @@
+#week01-2b.py
+#28. Find the Index of the First Occurrence in a String
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
